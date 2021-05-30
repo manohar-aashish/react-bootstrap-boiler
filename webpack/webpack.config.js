943f02b411563569c541879d5e42dfa0f296bbcf
Aashish Manohar Varlani <manohar.aashish@gmail.com>
2021-05-30 22:21:05 +0530

    Step 10: Added React

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ const { merge } = require('webpack-merge');
 const commonConfig = require(`./webpack.common.js`);
 module.exports = (envVars) => {
   const { env } = envVars;
-  const envConfig = require('./webpack.${env}.js');
+  const envConfig = require(`./webpack.${env}.js`);
   const config = merge(commonConfig, envConfig);
   return config;
 };
